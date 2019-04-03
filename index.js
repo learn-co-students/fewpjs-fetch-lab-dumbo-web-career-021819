1a625a1729d6568869f700447cb1664249a95ba9
brianryu <s.brianryu@gmail.com>
2019-04-03 11:47:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function fetchBooks() {
+function fetchBooks(resp) {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
 
 function renderBooks(json) {
