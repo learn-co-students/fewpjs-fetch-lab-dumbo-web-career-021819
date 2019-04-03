681834cb43c71f90031f6ea01c9c826420e579f9
char <25884226+char@users.noreply.github.com>
2019-04-03 15:03:30 -0400

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@
 // 	renderBooks(data)
 // }
 
-// async function fetchBooks() {
-// 	const resp = await fetch('https://anapioficeandfire.com/api/books')
-// 	const data = await resp.json()
-// 	renderBooks(data)
-// }
-
-function fetchBooks() {
-	fetch('https://anapioficeandfire.com/api/books')
-		.then(res => res.json())
-		.then(data => {
-			renderBooks(data)
-		})
+const fetchBooks = async () {
+	const resp = await fetch('https://anapioficeandfire.com/api/books')
+	const data = await resp.json()
+	renderBooks(data)
 }
+
+// function fetchBooks() {
+// 	fetch('https://anapioficeandfire.com/api/books')
+// 		.then(res => res.json())
+// 		.then(data => {
+// 			renderBooks(data)
+// 		})
+// }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
