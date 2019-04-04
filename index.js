db4c602cdfa420293484f45a570594f917095626
Nyette <antoinette.lee02@myhunter.cuny.edu>
2019-04-03 22:30:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 function fetchBooks() {
-}
+  const gameOfThronesUrl = "https://anapioficeandfire.com/api/books";
+  fetch(gameOfThronesUrl).then((response) => {
+    let json = response.json();
+    return json;
+  });
+};
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
