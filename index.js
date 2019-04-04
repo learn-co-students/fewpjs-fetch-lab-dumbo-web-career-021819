a1ca1c6028656959e7ef0b87ba7f8492079ae36b
mnkillebr <killebrew.biz@gmail.com>
2019-04-04 11:38:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response)=>{
+    return response.json()
+  }).then((bookPromise) => {renderBooks(bookPromise)})
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('body')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
